134ab475cb7528c1945f1fe08aae048fa67eaec2
Praveen <praveen744600@gmail.com>
2026-07-05 19:54:27 +0530

    Fix server.js

--- Backend/server.jsx
+++ Backend/server.jsx
@@ -4,21 +4,32 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-const cors = require("cors");
-
-const cors = require("cors");
 
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:5174"],
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "https://my-project-foodie-cape.vercel.app"
+  ],
   credentials: true
 }));
+
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
-.then(()=>console.log("MongoDB Connected"))
-.catch(err=>console.log(err));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
 
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/food", require("./routes/foodRoutes"));
 
-app.listen(5000, ()=>console.log("Server running on port 5000"));
+app.get("/", (req, res) => {
+  res.send("Foodie Cape Backend Running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
